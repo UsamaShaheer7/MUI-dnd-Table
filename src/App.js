16ce6d3f62ea39d1d8e247225dbd368bdafd200a
usama.s <usama.shaheer@tekhqs.com>
2024-05-27 17:10:59 +0500

    chnages

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import { DragDropContext, Draggable, Droppable } from "@hello-pangea/dnd";
 import { AiOutlineDrag } from "react-icons/ai";
 
 export default function BasicTable() {
@@ -18,66 +18,61 @@ export default function BasicTable() {
     createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
     createData("Eclair", 262, 16.0, 24, 6.0),
     createData("Cupcake", 305, 3.7, 67, 4.3),
-    createData("Gingerbread", 356, 16.0, 49, 3.9),
+    createData("Gingerbread", 356, 16.0, 49, 3.9)
   ]);
 
-  const handleDragEnd = (result) => {
-    if (!result.destination) return;
-    const items = Array.from(rows);
-    const [reorderedItem] = items.splice(result.source.index, 1);
-    items.splice(result.destination.index, 0, reorderedItem);
-
-    setRows(items);
+  const handleDragEnd = (e) => {
+    if (!e.destination) return;
+    let tempData = Array.from(rows);
+    let [source_data] = tempData.splice(e.source.index, 1);
+    tempData.splice(e.destination.index, 0, source_data);
+    setRows(tempData);
   };
-
   return (
     <DragDropContext onDragEnd={handleDragEnd}>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell></TableCell>
-              <TableCell>Dessert (100g serving)</TableCell>
-              <TableCell align="right">Calories</TableCell>
-              <TableCell align="right">Fat&nbsp;(g)</TableCell>
-              <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-              <TableCell align="right">Protein&nbsp;(g)</TableCell>
-            </TableRow>
-          </TableHead>
-          <Droppable droppableId="droppable-1">
-            {(provided) => (
-              <TableBody
-                {...provided.droppableProps}
-                ref={provided.innerRef}
-              >
-                {rows.map((row, index) => (
-                  <Draggable key={row.name} draggableId={row.name} index={index}>
-                    {(provided) => (
-                      <TableRow
-                        ref={provided.innerRef}
-                        {...provided.draggableProps}
-                        sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Dessert (100g serving)</TableCell>
+            <TableCell align="right">Calories</TableCell>
+            <TableCell align="right">Fat&nbsp;(g)</TableCell>
+            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+          </TableRow>
+        </TableHead>
+        <Droppable droppableId="droppable-1">
+          {(provider) => (
+            <TableBody ref={provider.innerRef} {...provider.droppableProps}>
+              {rows.map((row, index) => (
+                <Draggable key={row.name} draggableId={row.name} index={index}>
+                  {(provider) => (
+                    <TableRow
+                      key={row.name}
+                      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                      {...provider.draggableProps}
+                      ref={provider.innerRef}
+                    >
+                      <TableCell
+                        component="th"
+                        scope="row"
+                        {...provider.dragHandleProps}
                       >
-                        <TableCell {...provided.dragHandleProps}>
-                          <AiOutlineDrag />
-                        </TableCell>
-                        <TableCell component="th" scope="row">
-                          {row.name}
-                        </TableCell>
-                        <TableCell align="right">{row.calories}</TableCell>
-                        <TableCell align="right">{row.fat}</TableCell>
-                        <TableCell align="right">{row.carbs}</TableCell>
-                        <TableCell align="right">{row.protein}</TableCell>
-                      </TableRow>
-                    )}
-                  </Draggable>
-                ))}
-                {provided.placeholder}
-              </TableBody>
-            )}
-          </Droppable>
-        </Table>
-      </TableContainer>
+                        <AiOutlineDrag />
+                      </TableCell>
+                      <TableCell>{row.name}</TableCell>
+                      <TableCell align="right">{row.calories}</TableCell>
+                      <TableCell align="right">{row.fat}</TableCell>
+                      <TableCell align="right">{row.carbs}</TableCell>
+                      <TableCell align="right">{row.protein}</TableCell>
+                    </TableRow>
+                  )}
+                </Draggable>
+              ))}
+              {provider.placeholder}
+            </TableBody>
+          )}
+        </Droppable>
+      </Table>
     </DragDropContext>
   );
 }
